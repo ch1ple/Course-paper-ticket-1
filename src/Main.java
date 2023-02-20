13e7088e8cef7ba9b00cde3adb16522e0e3b6d34
ch1ple <teavis@mail.ru>
2023-02-20 20:59:47 +0300

    Course paper 1 theor part ticket 1 complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100) + 1;
+        }
+        return arr;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = generateRandomArray();
+        System.out.println(Arrays.toString(arr));
+        for (int i = arr.length - 1; i >= 0; i = i - 1) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
